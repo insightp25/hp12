a45d489c63bfc0d26b7d2f0438783c47874203f5
JO <71416000+insightp25@users.noreply.github.com>
2024-07-26 03:39:47 +0900

    reservation status 추가

--- src/main/java/io/clean/tdd/hp12/domain/reservation/model/Reservation.java
+++ src/main/java/io/clean/tdd/hp12/domain/reservation/model/Reservation.java
@@ -1,6 +1,7 @@
 package io.clean.tdd.hp12.domain.reservation.model;
 
 import io.clean.tdd.hp12.domain.concert.model.Seat;
+import io.clean.tdd.hp12.domain.reservation.enums.ReservationStatus;
 import io.clean.tdd.hp12.domain.user.model.User;
 import lombok.Builder;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Builder
 public record Reservation(
     long id,
+    ReservationStatus status,
     LocalDateTime createdAt,
     Seat seat,
     User user
