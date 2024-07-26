5098063ad802d2ede7e6c222e84ca14051d3328e
JO <71416000+insightp25@users.noreply.github.com>
2024-07-27 07:24:00 +0900

    콘서트 비즈니스 로직 작성

--- src/main/java/io/clean/tdd/hp12/domain/concert/ConcertService.java
+++ src/main/java/io/clean/tdd/hp12/domain/concert/ConcertService.java
@@ -1,10 +1,31 @@
 package io.clean.tdd.hp12.domain.concert;
 
+import io.clean.tdd.hp12.domain.concert.model.Concert;
+import io.clean.tdd.hp12.domain.concert.model.Seat;
+import io.clean.tdd.hp12.domain.concert.port.ConcertRepository;
+import io.clean.tdd.hp12.domain.concert.port.ConcertTitleRepository;
+import io.clean.tdd.hp12.domain.concert.port.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ConcertService {
 
+    private final ConcertRepository concertRepository;
+    private final ConcertTitleRepository concertTitleRepository;
+    private final SeatRepository seatRepository;
+
+    public List<Concert> findConcerts(long concertTitleId) {
+        return concertRepository.findByConcertTitleId(concertTitleId);
+    }
+
+    public List<Seat> findSeats(long concertTitleId, LocalDateTime occasion) {
+        Concert concert = concertRepository.findByConcertTitleIdAndOccasion(concertTitleId, occasion);
+
+        return seatRepository.findByConcertId(concert.id());
+    }
 }
