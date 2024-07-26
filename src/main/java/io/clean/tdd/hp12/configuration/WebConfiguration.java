12101d7c2970434e28285fc756b4936d3244ebd2
JO <71416000+insightp25@users.noreply.github.com>
2024-07-27 00:48:40 +0900

    대기열용 interceptor 설정 추가

---
+++ src/main/java/io/clean/tdd/hp12/configuration/WebConfiguration.java
@@ -0,0 +1,31 @@
+package io.clean.tdd.hp12.configuration;
+
+import io.clean.tdd.hp12.interfaces.common.WaitingQueueInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final WaitingQueueInterceptor waitingQueueInterceptor;
+
+    private final List<String> pathPatternsToAdd = List.of(
+        "/**"
+    );
+
+    private final List<String> pathPatternsToExclude = List.of(
+    );
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+            .addInterceptor(waitingQueueInterceptor)
+            .addPathPatterns(pathPatternsToAdd)
+            .excludePathPatterns(pathPatternsToExclude);
+    }
+}
