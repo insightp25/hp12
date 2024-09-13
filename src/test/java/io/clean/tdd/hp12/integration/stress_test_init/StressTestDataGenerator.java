f66de0a6efa0dda0aa23a891131cb698e972e582
JO <71416000+insightp25@users.noreply.github.com>
2024-09-13 19:19:23 +0900

    부하 테스트 초기화 데이터 생성 및 API 콜 로직 추가2

---
+++ src/test/java/io/clean/tdd/hp12/integration/stress_test_init/StressTestDataGenerator.java
@@ -0,0 +1,22 @@
+package io.clean.tdd.hp12.integration.stress_test_init;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@RequiredArgsConstructor
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class StressTestDataGenerator {
+
+    private final StressTestDataGenerationHelper stressTestDataGenerationHelper;
+
+    @Disabled // 부하 테스트시에 한하여 개별 수행
+    @Test
+    void 부하_테스트용_사전_데이터를_저장할_수_있다() {
+        stressTestDataGenerationHelper.bulkInsertDataForStressTest();
+    }
+}
