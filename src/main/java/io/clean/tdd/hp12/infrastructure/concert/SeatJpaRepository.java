362d041c34a7a43f8861a53bff6d841d324b1787
JO <71416000+insightp25@users.noreply.github.com>
2024-09-24 20:30:40 +0900

    dev: pessimistic lock 적용

--- src/main/java/io/clean/tdd/hp12/infrastructure/concert/SeatJpaRepository.java
+++ src/main/java/io/clean/tdd/hp12/infrastructure/concert/SeatJpaRepository.java
@@ -1,12 +1,15 @@
 package io.clean.tdd.hp12.infrastructure.concert;
 
 import io.clean.tdd.hp12.infrastructure.concert.entity.SeatEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface SeatJpaRepository extends JpaRepository<SeatEntity, Long> {
     List<SeatEntity> findByConcertEntity_Id(long concertId);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     SeatEntity findByConcertEntity_IdAndSeatNumber(long concertId, int seatNumber);
 }
